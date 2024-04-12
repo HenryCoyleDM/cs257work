2a30053e7ec4d4bdb47723d281b60ac6844a8fe0
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-04-12 10:31:00 -0500

    Selecting real values of city/state populations

--- learn_more_sql/find_largest_proportion_city.sql
+++ learn_more_sql/find_largest_proportion_city.sql
@@ -1,1 +1,2 @@
-SELECT * FROM us_cities JOIN us_states ON us_cities.state_name = us_states.state_name;
+CREATE VIEW cities_plus_states_populations AS SELECT * FROM us_cities JOIN us_states ON us_cities.state_name = us_states.state_name;
+SELECT city_name, state_name, CAST(city_population AS REAL) AS real_city_population, CAST(state_population AS REAL) AS real_state_population FROM cities_plus_states_populations;

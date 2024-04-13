6345e9ccc1b003e92a0f7905cdef3983e09f4f8a
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-04-12 21:25:03 -0500

    Added math for finding the proportions of populations. Named auxillary tables. Printing results in sorted order.

--- learn_more_sql/find_largest_proportion_city.sql
+++ learn_more_sql/find_largest_proportion_city.sql
@@ -1,2 +1,4 @@
-CREATE VIEW cities_plus_states_populations AS SELECT * FROM us_cities JOIN us_states ON us_cities.state_name = us_states.name;
-SELECT city_name, state_name, CAST(city_population AS REAL) AS real_city_population, CAST(state_population AS REAL) AS real_state_population FROM cities_plus_states_populations;
+CREATE VIEW cities_plus_states_populations AS SELECT * FROM us_cities JOIN us_states ON us_cities.state_name = us_states.state_name2;
+CREATE VIEW real_populations AS SELECT city_name, state_name, CAST(city_population AS REAL) AS real_city_population, CAST(state_population AS REAL) AS real_state_population, FROM cities_plus_states_populations;
+CREATE VIEW population_quotients AS SELECT city_name, (real_city_population / real_state_population) AS quotient FROM real_populations;
+SELECT * FROM population_quotients ORDER BY quotient;

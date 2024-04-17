322a41f21e8a524535b704f86705dc623e2e83a7
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-04-17 10:46:51 -0500

    Added a script for querying the database for cities

---
+++ python_sql/city_query.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+connection = psycopg2.connect(
+    host = "localhost",
+    port = 5432,
+    database = "coyleh2",
+    user = "coyleh2",
+    password = "books257beach"
+)
+if connection is None:
+    print("Connection failed")
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM us_cities WHERE city_name LIKE 'Northfield' AND state_name LIKE 'Minnesota'")
+row = cursor.fetchone()
+if row is None:
+    print("Northfield is not present in the database")
+else:
+    print(row)

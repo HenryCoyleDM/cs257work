b0c9f0379cd19686b071e1143e42ba25345427d2
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-04-22 10:41:44 -0500

    added functionality for adding two integers

--- flasklab/web_responder.py
+++ flasklab/web_responder.py
@@ -4,18 +4,15 @@ import flask
 
 app = flask.Flask(__name__)
 
-@app.route('/hello')
-def my_function():
-    return "Hello World!"
+@app.route('/add/<number1>/<number2>')
+def my_display(number1, number2):
+    try:
+        num1 = int(number1)
+        num2 = int(number2)
+        return str(num1 + num2)
+    except ValueError:
+        return '<p style="color:Red">Please input 2 integers</p>'
 
-@app.route('/display/<word1>/<word2>')
-def my_display(word1, word2):
-    the_string = "The words are: " + word1 + " and " + word2;
-    return the_string
-
-@app.route('/color/<word1>')
-def my_color(word1):
-    return '<h1 style="color:Red">' + word1 + '</h1>'
 
 if __name__ == '__main__':
     my_port = 5113

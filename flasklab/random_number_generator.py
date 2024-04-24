aea67015c2210c44554a1082ef5539e4e931bbf2
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-04-24 15:42:51 -0500

    Hopefully fixed buttons in index.html

--- flasklab/random_number_generator.py
+++ flasklab/random_number_generator.py
@@ -9,26 +9,13 @@ app = Flask(__name__)
 
 # adapted from https://pythonbasics.org/flask-tutorial-routes/
 @app.route('/random', methods=["POST", "GET"])
-def index():
+def generate_random_number(argument_low=None, argument_high=None):
     if request.method == "POST":
         low = request.form["low"]
         high = request.form["high"]
         return "low: %s, high: %s" % (low, high)
     else:
         return render_template("index.html")
-
-@app.route('/random/<low>/<high>')
-def random(low, high):
-    return "a random number from %s to %s" % (low, high)
-    return render_template("random.html", randNum=num, low=low, high=high)
-
-@app.route('/random/<arguments>')
-def single_argument(arguments):
-    return arguments
-
-@app.route('/random?<values>')
-def form_values(values):
-    return "these are values: %s" % (values)
 
 if __name__ == '__main__':
     my_port = 5113

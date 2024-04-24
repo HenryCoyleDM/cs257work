b93e29f16f358f1d875111c66caf95a17bc202f0
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-04-24 18:39:52 -0500

    Reverted changes back to using POST

--- flasklab/random_number_generator.py
+++ flasklab/random_number_generator.py
@@ -8,19 +8,18 @@ import random
 app = Flask(__name__)
 
 # adapted from https://pythonbasics.org/flask-tutorial-routes/
-@app.route('/random') # @app.route('/random', methods=["POST", "GET"])
-def generate_random_number(low, high):
-    print("recieved low: %s, high: %s" % (low, high))
-    if low is not None and high is not None:
+@app.route('/random', methods=["POST", "GET"])
+def generate_random_number():
+    # if argument_low is not None and argument_high is not None:
+        # return "low: %s, high: %s" % (argument_low, argument_high)
+    # else:
+        # return render_template("index.html")
+    if request.method == "POST":
+        low = request.form["low"]
+        high = request.form["high"]
         return "low: %s, high: %s" % (low, high)
     else:
         return render_template("index.html")
-    # if request.method == "POST":
-        # low = request.form["low"]
-        # high = request.form["high"]
-        # return "low: %s, high: %s" % (low, high)
-    # else:
-        # return render_template("index.html")
 
 if __name__ == '__main__':
     my_port = 5113

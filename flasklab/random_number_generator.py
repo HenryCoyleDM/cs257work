db4ce02b8b9a418c0c2f49defcbdde1afba7d79e
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-04-24 15:47:15 -0500

    Hopefully simplified getting the values entered, putting them in the URL

--- flasklab/random_number_generator.py
+++ flasklab/random_number_generator.py
@@ -8,14 +8,18 @@ import random
 app = Flask(__name__)
 
 # adapted from https://pythonbasics.org/flask-tutorial-routes/
-@app.route('/random', methods=["POST", "GET"])
-def generate_random_number(argument_low=None, argument_high=None):
-    if request.method == "POST":
-        low = request.form["low"]
-        high = request.form["high"]
+@app.route('/random') # @app.route('/random', methods=["POST", "GET"])
+def generate_random_number(low=None, high=None):
+    if low is not None and high is not None:
         return "low: %s, high: %s" % (low, high)
     else:
         return render_template("index.html")
+    # if request.method == "POST":
+        # low = request.form["low"]
+        # high = request.form["high"]
+        # return "low: %s, high: %s" % (low, high)
+    # else:
+        # return render_template("index.html")
 
 if __name__ == '__main__':
     my_port = 5113

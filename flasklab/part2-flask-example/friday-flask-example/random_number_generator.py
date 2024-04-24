5c865354d55a316db69fb6632fcd8f9f5d35032e
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-04-24 10:21:55 -0500

    Fixed typos, added an option for rerolling the same random number range

--- flasklab/part2-flask-example/friday-flask-example/random_number_generator.py
+++ flasklab/part2-flask-example/friday-flask-example/random_number_generator.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import render_template
 import random
 
+# sample code from Matthew Lepinski
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,8 +18,8 @@ def rand(low, high):
     high_int = int(high)
     
     num = random.randint(low_int, high_int)
-    return render_template("random.html", randNum = num)
+    return render_template("random.html", randNum=num, low=low, high=high)
 
 if __name__ == '__main__':
     my_port = 5113
-    app.run(host='0.0.0.0', port = my_port) 
+    app.run(host='0.0.0.0', port = my_port)

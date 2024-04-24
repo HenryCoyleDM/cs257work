79bcd2f6850053edb0881b6a6b53f9c64ce4a97d
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-04-24 10:36:53 -0500

    Adding functionality for inputting your own bounds, rearanged pages to use arguments with ?

--- flasklab/part2-flask-example/friday-flask-example/random_number_generator.py
+++ flasklab/part2-flask-example/friday-flask-example/random_number_generator.py
@@ -6,18 +6,9 @@ import random
 
 app = Flask(__name__)
 
-@app.route('/')
-def welcome():
-    return render_template("index.html")
-
-@app.route('/rand/<low>/<high>')
-def rand(low, high):
-    #Input values that come from a URL (i.e., @app.route)
-    #   are always strings so I need to convert the type to int
-    low_int = int(low)
-    high_int = int(high)
-    
-    num = random.randint(low_int, high_int)
+@app.route('/random<arguments>')
+def rand(arguments):
+    return arguments
     return render_template("random.html", randNum=num, low=low, high=high)
 
 if __name__ == '__main__':

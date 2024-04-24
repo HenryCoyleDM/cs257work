e4ab34204ca9a75a907ef09e91694c56b33c17cd
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-04-24 10:53:46 -0500

    added some debugging in order to discover how to use the form function in HTML

--- flasklab/part2-flask-example/friday-flask-example/random_number_generator.py
+++ flasklab/part2-flask-example/friday-flask-example/random_number_generator.py
@@ -6,10 +6,18 @@ import random
 
 app = Flask(__name__)
 
-@app.route('/random/<arguments>')
-def rand(arguments):
-    return arguments
+@app.route('/random')
+def index():
+    return render_template("index.html")
+
+@app.route('/random/<low>/<high>')
+def random(low, high):
+    return "a random number from %s to %s" % (low, high)
     return render_template("random.html", randNum=num, low=low, high=high)
+
+@app.route('/random/<arguments>')
+def single_argument(argument):
+    return argument
 
 if __name__ == '__main__':
     my_port = 5113

623657e4fd9884d73a125f43acdb6c19562ac7b6
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-04-26 10:30:36 -0500

    Added function for generating a random US city, weighted by population, using the cities database

--- flasklab/random_number_generator.py
+++ flasklab/random_number_generator.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flask import request
 from flask import url_for
 import random
+import psycopg2
 
 # sample code from Matthew Lepinski
 
@@ -19,12 +20,17 @@ def generate_random_number():
     if request.method == "POST":
         low = request.form["low"]
         high = request.form["high"]
+        special = request.form["special"]
         print(f"Recieved POST request: {low} to {high}")
     else:
         # adapted from https://pythonbasics.org/flask-tutorial-routes/
         low = request.args.get("low")
         high = request.args.get("high")
+        special = request.args.get("special")
         print(f"Recieved GET request. Arguments are {request.args}")
+    if special == "city":
+        random_city = get_random_city_by_population()
+        return f"{random_city[0]}, {random_city[1]}"
     if low is None or high is None:
         return render_template("index.html", additional_body="")
     else:
@@ -35,6 +41,31 @@ def generate_random_number():
             return render_template("random.html", random_value=random_int, reroll_bound_url=rerolling_url)
         except ValueError:
             return render_template("index.html", additional_body="Please enter 2 integers")
+
+# gets a random US city from the database, weighted by population
+def get_random_city_by_population():
+    # adapted from sample code by Matthew Lepinski
+    connection = psycopg2.connect(
+        host = "localhost",
+        port = 5432,
+        database = "coyleh2",
+        user = "coyleh2",
+        password = "books257beach"
+    )
+    if connection is None:
+        print("Connection failed")
+    cursor = connection.cursor()
+    # adapted from https://learnsql.com/cookbook/how-to-sum-values-of-a-column-in-sql/
+    cursor.execute("SELECT SUM(city_population) AS total_population FROM us_cities")
+    total_population = cursor.fetchone()
+    cursor.execute("SELECT * FROM us_cities ORDER BY city_population DESC")
+    all_cities = cursor.fetchall()
+    random_resident = random.randint(1, total_population)
+    for city in all_cities:
+        random_resident -= city[2]
+        if random_resident <= 0:
+            return city
+    return None
 
 if __name__ == '__main__':
     my_port = 5113

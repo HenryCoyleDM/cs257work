a39587e9c1919b9e859dc4efcbddd1ebdf0c0526
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-05-08 10:19:02 -0500

    Added starter HTML, flask, CSS, and Javascript files. Made some text display in monospace font

---
+++ csslab/home_page_flask.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/minesweeper')
+def display_minesweeper_game():
+    return render_template("minesweeper.html")
+
+if __name__ == '__main__':
+    my_port = 5113
+    app.run(host='0.0.0.0', port = my_port)

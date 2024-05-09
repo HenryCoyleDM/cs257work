6608f60e8d472970cbdd1776116dab759a97e3ff
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-05-08 21:49:15 -0500

    Testing whether Javascript can store variables between events with an arbitrary incrementer

--- csslab/static/minesweeper.js
+++ csslab/static/minesweeper.js
@@ -1,5 +1,10 @@
+incrementer = 0;
+
 function display_coordinates_of_click(x, y) {
     display_paragraph = document.getElementById("coordinate_display");
     display_paragraph.innerHTML = x + ", " + y;
+    incrementer += x + y;
     console.log("A letter was clicked: " + x + ", " + y);
+    display_incrementer = document.getElementById("incrementer_display");
+    display_incrementer.innerHTML = incrementer;
 }

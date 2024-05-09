e7a947fce9d7e0ef33b5a56f573a2881e143b69d
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-05-08 21:10:25 -0500

    Changed test to dispay 3 lines

--- csslab/minesweeper.py
+++ csslab/minesweeper.py
@@ -12,7 +12,7 @@ def get_test_html():
             + render_template("two.html") + render_template("three.html") + render_template("four.html")\
             + render_template("five.html") + render_template("six.html") + render_template("seven.html")\
             + render_template("eight.html") + render_template("flag.html")
-    return render_template("head.html") + line1 + "<br>" + line1
+    return render_template("head.html") + line1 + "<br>" + line1 + "<br>" + render_template("blank.html") + render_template("blank.html") + line1
 
 if __name__ == '__main__':
     my_port = 5113

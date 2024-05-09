6e7b6861d7bad9de815fe335d67e2b49c2ca806d
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-05-08 21:44:53 -0500

    Fixed selecting display field

--- csslab/static/minesweeper.js
+++ csslab/static/minesweeper.js
@@ -1,5 +1,5 @@
 function display_coordinates_of_click() {
-    display_paragraph = display.coordinate_display;
+    display_paragraph = document.getElementById("coordinate_display");
     display_paragraph.innerHTML = Math.random() + ", " + Math.random();
     console.log("A letter was clicked");
 }

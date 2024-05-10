d6128ba8a8d70dfe09021c013226468e2258d30c
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-05-10 16:21:37 -0500

    Added functionality for creating a grid of HTML elements and referencing them in a JS array, along with the values of each cell. Wrote a test to create an alternating colored grid.

--- csslab/minesweeper.py
+++ csslab/minesweeper.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/minesweeper')
 def display_minesweeper_game():
-    return get_cell_click_test_HTML()
+    return get_empty_grid_of_cells()
 
 def get_characters_test_HTML():
     line1 = render_template("blank.html") + render_template("zero.html") + render_template("one.html")\
@@ -15,7 +15,13 @@ def get_characters_test_HTML():
     return render_template("head.html") + line1 + "<br>" + line1 + render_template("blank.html") + render_template("blank.html") + "<br>" + render_template("blank.html") + render_template("blank.html") + line1
 
 def get_cell_click_test_HTML():
-    return render_template("minesweeper.html")
+    return render_template("click_test.html")
+
+def create_blank_block_of_cells():
+    return '<div id="cell_grid" onload="instantiate_field()"></div>'
+
+def get_empty_grid_of_cells():
+    return render_template("head.html") + create_blank_block_of_cells()
 
 if __name__ == '__main__':
     my_port = 5113
--- csslab/static/minesweeper.js
+++ csslab/static/minesweeper.js
@@ -1,4 +1,28 @@
-incrementer = 0;
+const grid_width = 20;
+const grid_height = 10;
+
+// grid of cells. each cell contains an object containing the cell's value along with a reference to the character that is displaying that cell
+grid = new Array(grid_width * grid_height);
+
+function instantiate_field() {
+    cell_grid_div = document.getElementById("cell_grid");
+    for (y=0; y<grid_height; y++) {
+        for (x=0; x<grid_width; x++) {
+            new_cell_html = document.createElement("span");
+            value = Math.floor(Math.random() * 10);
+            assign_symbol_and_colors_to_HTML_cell(value, new_cell_html);
+            cell_grid_div.append_child(new_cell_html);
+            grid[x + y * grid_width] = {value: value, element: new_cell_html};
+        }
+    }
+}
+
+is_red = true;
+function assign_symbol_and_colors_to_HTML_cell(value, cell) {
+    cell.innerHTML = toString(value);
+    cell.style.color = (is_red ? "red" : "blue");
+    is_red = !is_red;
+}
 
 function display_coordinates_of_click(x, y) {
     display_paragraph = document.getElementById("coordinate_display");

60cdfc40ae1ef99790e8be7e0c7bb010e7ccb384
HenryCoyleDM <henry.coyle65536@gmail.com>
2024-05-12 20:50:39 -0500

    Fixed syntax error

--- csslab/static/minesweeper.js
+++ csslab/static/minesweeper.js
@@ -71,10 +71,10 @@ function display_coordinates_of_click(x, y) {
 function cell_is_clicked(x, y, event) {
     // https://stackoverflow.com/questions/2405771/is-right-click-a-javascript-event
     var is_right_mouse_button;
-    if ("which" in e) {
-        is_right_mouse_button = e.which == 3;
-    } else if ("button" in e) {
-        is_right_mouse_button = e.button == 2;
+    if ("which" in event) {
+        is_right_mouse_button = event.which == 3;
+    } else if ("button" in event) {
+        is_right_mouse_button = event.button == 2;
     }
     console.log((is_right_mouse_button ? "Right clicked (" : "Clicked (")+x+", "+y+")");
 }
